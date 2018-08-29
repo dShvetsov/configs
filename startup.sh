027a667018364c4ec5182445b5160cb80653d038
dShvetsov <dshvetsov@arccn.ru>
2018-08-29 15:10:53 +0300

    startup file

---
+++ startup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ $USER -ne 0 ]]
+then
+    echo "run as root"
+    exit 1
+fi
+
+echo "Start settings system"
+
+echo "Install softs..."
+
+apt update && \
+apt install -y vim git shutter openssh-server zsh cmake libboost-all-dev doxygen \
+            chromium-browser cppman zsh screen tmux && \
+
+
+# Generate ssh keys
+su  -c "ssh-keygen -t rsa -b 4096 -C \"rbbozk@yandex.ru\""  dshvetsov && \
+echo "Use this public key into github and bitbucket accout" && \
+cat .ssh/id_rsa.pub && \
+sleep 2
+
+echo "make zsh dedault"
+chsh -s $(which zsh)
+
+echo "installing vim plugin manager ... "
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+echo "execute pathogen#infect()" >> .vimrc
+
+echo "installing  NERDTree... "
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree && \
+echo "map <C-n> :NERDTreeToggle<CR>" >> .vimrc
+
+echo "installing YouCompleteMe... "
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe && \
+cd ~/.vim/bundle/YouCompleteMe && \
+git submodule update --init --recursive && \
+./install.py --clang-completer
+
+echo "installing cppman into vim"
+echo "autocmd FileType cpp set keywordprg=cppman" >> .vimrc
+echo "running into screen caching cppman lists"
+screen -dmS cppman_caching "y | cppman -c"
